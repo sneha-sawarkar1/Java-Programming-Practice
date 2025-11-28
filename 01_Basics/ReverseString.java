49fd81452e8358ec2b3702fbcb2140b0fba4588b
sneha-sawarkar1 <sneha.sawarkar18@gmail.com>
2025-11-28 16:49:32 +0530

    Added ReverseString program under Basics

---
+++ 01_Basics/ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Sneha";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + rev);
+    }
+}
