54f7856a55195c61ee13c842a356c0ea5205957b
sneha-sawarkar1 <sneha.sawarkar18@gmail.com>
2025-11-28 21:19:36 +0530

    Create SwapTwoNumberWithTemproryVariable.java

---
+++ 01_Basics/SwapTwoNumberWithTemproryVariable.java
@@ -0,0 +1,16 @@
+public class SwapNumbersWithTemp {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before Swapping: a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swapping: a = " + a + ", b = " + b);
+    }
+}
