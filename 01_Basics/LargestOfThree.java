18ba8ab59286b3e2f92ba2bb66daae39c9e9c139
sneha-sawarkar1 <sneha.sawarkar18@gmail.com>
2025-11-28 21:17:02 +0530

    Create LargestOfThree.java

---
+++ 01_Basics/LargestOfThree.java
@@ -0,0 +1,15 @@
+public class LargestOfThree {
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 25, c = 7;
+
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest number");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest number");
+        } else {
+            System.out.println(c + " is the largest number");
+        }
+    }
+}
