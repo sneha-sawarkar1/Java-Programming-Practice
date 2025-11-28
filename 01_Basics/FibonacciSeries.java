036ac16c2949414b1bf93afe2620d7ee1ed1f5d3
sneha-sawarkar1 <sneha.sawarkar18@gmail.com>
2025-11-28 20:59:00 +0530

    Created FibonacciSeries.java

---
+++ 01_Basics/FibonacciSeries.java
@@ -0,0 +1,18 @@
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+
+        int n = 10; // number of terms
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
