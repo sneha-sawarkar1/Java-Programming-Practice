28f5a8dcaa1cc0cbc251c478a3268b47a7fc4d0e
Sneha sawarkar <sneha.sawarkar18@gmail.com>
2025-11-30 22:51:07 +0530

    Create EvenOddNumber.java

---
+++ 01_Basics/EvenOddNumber.java
@@ -0,0 +1,13 @@
+public class EvenOddNumber {
+
+    public static void main(String[] args) {
+
+        int num = 17;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is an Even Number");
+        } else {
+            System.out.println(num + " is an Odd Number");
+        }
+    }
+}
